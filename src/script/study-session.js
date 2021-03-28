de3db1cf4d8a7b0d7815019298968a4a93889d00
GabbyNEW <gabriel_marquez@dlsu.edu.ph>
2021-03-28 23:11:49 +0800

    add multi card incorporation

--- src/script/study-session.js
+++ src/script/study-session.js
@@ -1,5 +1,51 @@
 var flashcard = document.querySelector(".flashcard");
+var passBtn = document.getElementById("pass-btn");
+var failBtn = document.getElementById("fail-btn");
 
-flashcard.addEventListener("click", function() {
+// Nodes for editing flashcard eleements
+var frontWordNode = document.getElementById("front-word");
+var backWordNode = document.getElementById("back-word");
+var backDefinitionNode = document.getElementById("back-definition");
+
+// Card data for study session (hardcoded for now)
+var reviewCards = [
+	{
+		frontWord: "叶う",
+		backWord: "Kanau",
+		backDefinition: "[to come true (of a wish, prayer, etc.); to be realized; to be fulfilled]"
+	},
+	{
+		frontWord: "開く",
+		backWord: "Aku",
+		backDefinition: "To open (a door etc.)"
+	}];
+
+// track current card being studied
+var currentCardIndex = 0;
+var maxCards = 2;
+
+// Initialize review for first card
+frontWordNode.innerHTML = reviewCards[currentCardIndex].frontWord;
+backWordNode.innerHTML = reviewCards[currentCardIndex].backWord;
+backDefinitionNode.innerHTML = reviewCards[currentCardIndex].backDefinition;
+
+flashcard.addEventListener('click', function() {
 	flashcard.classList.toggle('flip');
 })
+
+flashcard.addEventListener('click', function() {
+	flashcard.classList.remove("animate__fadeInRight");
+});
+
+passBtn.addEventListener('click', function() {
+	if (currentCardIndex < maxCards - 1) {
+		flashcard.classList.add("animate__fadeInRight");
+		currentCardIndex += 1;
+		frontWordNode.innerHTML = reviewCards[currentCardIndex].frontWord;
+		backWordNode.innerHTML = reviewCards[currentCardIndex].backWord;
+		backDefinitionNode.innerHTML = reviewCards[currentCardIndex].backDefinition;
+	}
+	else {
+		flashcard.classList.add("animate__fadeOutLeft");
+	}
+});
