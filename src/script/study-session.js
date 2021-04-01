a05cf98af9b61fd4444fe5148198ecf0bbd6a04b
GabbyNEW <gabriel_marquez@dlsu.edu.ph>
2021-04-02 01:08:07 +0800

    implement finish message

--- src/script/study-session.js
+++ src/script/study-session.js
@@ -1,6 +1,10 @@
 var flashcard = document.querySelector(".flashcard");
+var btnContainer = document.getElementById('btn-container');
 var passBtn = document.getElementById("pass-btn");
 var failBtn = document.getElementById("fail-btn");
+var finishMessages = document.getElementById('study-complete-msg-container');
+var finishMessage1 = document.getElementById('msg-1');
+var finishMessage2 = document.getElementById('msg-2');
 
 // Nodes for editing flashcard eleements
 var frontWordNode = document.getElementById("front-word");
@@ -21,6 +25,7 @@ var reviewCards = [
 	}];
 
 // track current card being studied
+var hasFinishedStudying = false;
 var currentCardIndex = 0;
 var maxCards = 2;
 
@@ -39,10 +44,16 @@ flashcard.addEventListener('animationend', function() {
 		flashcard.classList.remove("animate__fadeInRight");
 		isFadeIn = false;
 	}
+	else if (hasFinishedStudying) {
+		flashcard.classList.add('is-hidden');
+		finishMessages.classList.remove('is-hidden');
+		finishMessages.classList.add('animate__bounceIn');
+	}
 });
 
 passBtn.addEventListener('click', function() {
 	if (currentCardIndex < maxCards - 1) {
+		flashcard.classList.remove('flip');
 		currentCardIndex += 1;
 		frontWordNode.innerHTML = reviewCards[currentCardIndex].frontWord;
 		backWordNode.innerHTML = reviewCards[currentCardIndex].backWord;
@@ -52,5 +63,7 @@ passBtn.addEventListener('click', function() {
 	}
 	else {
 		flashcard.classList.add("animate__fadeOutLeft");
+		btnContainer.classList.add('animate__fadeOutDown');
+		hasFinishedStudying = true;
 	}
 });
