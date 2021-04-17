1a5a45fef68a094440b7a5804d1407ae500e8d86
GabbyNEW <gabriel_marquez@dlsu.edu.ph>
2021-04-18 00:46:54 +0800

    add deck setting schema

--- models/flashcards.js
+++ models/flashcards.js
@@ -16,14 +16,28 @@ var getSchema = {
         return FlashcardSchema;
     },
 
+    // Contains all decks of a user
     FlashcardInfoSchema : function(username) {
         const FlashcardInfoSchema = new mongoose.Schema({
-            Tag: String,
+            Tag: String, // Index
             decks: Object
         });
 
         return FlashcardInfoSchema;
+    },
+
+    // Get preferences for a deck created by a user
+    DeckSettingSchema : function(username) {
+        const DeckSettingSchema = new mongoose.Schema({
+            Tag: String, // Deck Settings
+            Deck: String,
+            MaxReviews: Number,
+            MaxNew: Number
+        });
+
+        return DeckSettingSchema;
     }
+
 }
 
 module.exports = getSchema;
