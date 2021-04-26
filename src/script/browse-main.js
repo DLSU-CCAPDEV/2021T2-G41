505cb9b6575ae38393462a7651924a4b487f09e2
GabbyNEW <gabriel_marquez@dlsu.edu.ph>
2021-04-26 19:18:13 +0800

    combine two files to one

--- src/script/browse-main.js
+++ src/script/browse-main.js
@@ -15,6 +15,71 @@ var addCardModal = document.getElementById("modal-addCard-container");
 var addCard_onModal = document.getElementById("addCard-btn");
 var addCard_offModal = document.getElementById("modal-addCard-btn-close");
 
+var browseTable = document.getElementById('browse-table');
+var row, front, back, deck, date; 
+
+// Card data (hardcoded for now) [Front, Back, Deck, Date]
+var tempCardData /*harcoded for now*/ = [
+    {
+        front: "切る",
+        back: "Kiru<br>To Cut",
+        deck: "Core 2K Japanese Vocabulary",
+        date: "12-May-21"
+    },
+    {
+        front: "閉める",
+        back: "Shimeru<br>To close something (intransitive verb)",
+        deck: "Core 2K Japanese Vocabulary",
+        date: "24-May-21"
+    },
+    {
+        front: "食べる",
+        back: "Taberu<br>To eat something",
+        deck: "Core 2K Japanese Vocabulary",
+        date: "24-May-21"
+    },
+    {
+        front: "閉める",
+        back: "Shimeru<br>To close something (intransitive verb)",
+        deck: "Core 2K Japanese Vocabulary",
+        date: "24-May-21"
+    },
+    {
+        front: "届ける",
+        back: "Todokeru<br>To deliver something",
+        deck: "Core 2K Japanese Vocabulary",
+        date: "24-May-21"
+    },
+    {
+        front: "慰める",
+        back: "Nagusameru<br>To comfort",
+        deck: "Core 2K Japanese Vocabulary",
+        date: "16-May-21"
+    },
+    {
+        front: "話す",
+        back: "Hanasu<br>To talk, to discourse (e.g story)",
+        deck: "Core 2K Japanese Vocabulary",
+        date: "24-May-21"
+    }
+    ];
+
+
+// fill table with all flashcard data (may be filtered)
+tempCardData.forEach(function(currCard) {
+    row = browseTable.insertRow();
+    front = row.insertCell();
+    front.classList.add("browse-table-front-data");
+    front.innerHTML = currCard.front;
+    back = row.insertCell();
+    back.classList.add("browse-table-date-data");
+    back.innerHTML = currCard.back;
+    deck = row.insertCell();
+    deck.innerHTML = currCard.deck;
+    date = row.insertCell();
+    date.innerHTML = currCard.date;
+});
+
 // Row selected event handler
 for (var i = 1; i < browse_table.rows.length; i++) {
     browse_table.rows[i].onclick = function() {
