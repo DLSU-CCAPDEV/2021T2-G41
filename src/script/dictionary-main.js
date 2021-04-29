4272dbfdf59d8cd4554afbca6ba96649a848f3d4
GabbyNEW <gabriel_marquez@dlsu.edu.ph>
2021-04-29 15:09:18 +0800

    show ENG translation when JP text clicked

--- src/script/dictionary-main.js
+++ src/script/dictionary-main.js
@@ -1,5 +1,6 @@
-const addButtons = document.querySelector('.add-icon');
-const englishTranslationTexts = document.querySelector('english-sentence-example');
+const addButtons = document.querySelectorAll('.add-icon');
+const translationTexts = document.querySelectorAll('.japanese-sentence');
+const englishTranslationTexts = document.querySelectorAll('.english-sentence-example');
 
 // modal node
 const addSentenceModal = document.getElementById('add-sentence-modal');
@@ -8,12 +9,41 @@ const addSentenceModal = document.getElementById('add-sentence-modal');
 const closeBtn = document.getElementById('modal-close');
 const cancelBtn = document.getElementById('cancel-btn');
 
-// X button clicked
+function getTranslationRequest(sentence) {
+    return new Promise((resolve, reject) => {
+        let xhttp = new XMLHttpRequest();
+        let query = "sentence=" + sentence;
+
+        xhttp.open('GET', '/getEngTranslation?' + query, true);
+
+        xhttp.onload = () => {
+            console.log("Got English translation.");
+            resolve(xhttp.responseText);
+        }
+
+        xhttp.send();
+    });
+}
+
+// X button clicked event
 closeBtn.addEventListener('click', (e) => {
     addSentenceModal.classList.remove('is-active');
 });
 
-// Cancel button clicked
+// Cancel button clicked event
 cancelBtn.addEventListener('click', (e) => {
     addSentenceModal.classList.remove('is-active');
 });
+
+translationTexts.forEach((translationText, translationTextIndex) => {
+    translationText.addEventListener('click', async (e) => {
+        // show text (loading)
+        englishTranslationTexts[translationTextIndex].hidden = false;
+
+        // get English translation
+        let engTranslationText = await getTranslationRequest(translationText.innerHTML);
+
+        englishTranslationTexts[translationTextIndex].innerHTML = engTranslationText;
+        console.log(engTranslationText);
+    });
+})
