a68414af305581bc59dd89bd1549fd95184ab670
GabbyNEW <gabriel_marquez@dlsu.edu.ph>
2021-04-29 19:12:58 +0800

    add sample sentence to database

--- src/script/dictionary-main.js
+++ src/script/dictionary-main.js
@@ -1,10 +1,16 @@
+// activate modal, dictionary sentence nodes
 const addButtons = document.querySelectorAll('.add-icon');
 const translationTexts = document.querySelectorAll('.japanese-sentence');
 const englishTranslationTexts = document.querySelectorAll('.english-sentence-example');
 
-// modal node
+// modal field node
 const addSentenceModal = document.getElementById('add-sentence-modal');
+const modalFrontWord = document.getElementById('frontWord');
+const modalBackWord = document.getElementById('backWord');
+const deckSelect = document.getElementById('deckSelect');
 
+// modal button nodes
+const saveSentence = document.getElementById('save-btn');
 // activate/deactivate modal nodes
 const closeBtn = document.getElementById('modal-close');
 const cancelBtn = document.getElementById('cancel-btn');
@@ -27,14 +33,19 @@ function getTranslationRequest(sentence) {
 
 // X button clicked event
 closeBtn.addEventListener('click', (e) => {
+    modalFrontWord.value = "";
+    modalBackWord.value = "";
     addSentenceModal.classList.remove('is-active');
 });
 
 // Cancel button clicked event
 cancelBtn.addEventListener('click', (e) => {
+    modalFrontWord.value = "";
+    modalBackWord.value = "";
     addSentenceModal.classList.remove('is-active');
 });
 
+// On Japanese sentence click event, show translation
 translationTexts.forEach((translationText, translationTextIndex) => {
     translationText.addEventListener('click', async (e) => {
         // show text (loading)
@@ -46,4 +57,39 @@ translationTexts.forEach((translationText, translationTextIndex) => {
         englishTranslationTexts[translationTextIndex].innerHTML = engTranslationText;
         console.log(engTranslationText);
     });
-})
+});
+
+// On Add btn (add sentence to deck) click event, show modal
+addButtons.forEach((addButton, addButtonIndex) => {
+    addButton.addEventListener('click', async (e) => {
+        // activate modal
+        addSentenceModal.classList.add('is-active');
+
+        // Set textarea content (Front, Back)
+        modalFrontWord.value = translationTexts[addButtonIndex].innerHTML;
+
+        if (englishTranslationTexts[addButtonIndex].textContent == "Loading...") {
+            let translationText = await getTranslationRequest(translationTexts[addButtonIndex].textContent);
+            modalBackWord.value = translationText;
+        }
+        else 
+            modalBackWord.value = englishTranslationTexts[addButtonIndex].textContent;
+    });
+});
+
+saveSentence.addEventListener('click',  (e) => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', '/addCard', true);
+
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhttp.onload = function() {
+        modalFrontWord.value = "";
+        modalBackWord.value = "";
+        addSentenceModal.classList.remove('is-active');
+    };
+
+    let deckSelected = deckSelect.options[deckSelect.selectedIndex].text;
+
+    xhttp.send("front=" + modalFrontWord.value + "&back=" + modalBackWord.value + "&deck=" + deckSelected);
+});
