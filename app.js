56c500c88e1d3c60f60a5cd87c6efbb25d9b9149
GabbyNEW <gabriel_marquez@dlsu.edu.ph>
2021-05-07 21:21:32 +0800

    express-session middleware

--- app.js
+++ app.js
@@ -7,8 +7,26 @@ const app = express();
 const favicon = require('serve-favicon');
 const path = require('path');
 
-// // setup favicon
+// sessions app and mongoDB store
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+// setup favicon
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// setup session and cookies
+app.use(session({
+  secret: 'secret email', // also store this on an evironment variable
+  resave: true,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: "mongodb+srv://dbAdmin:admin123@kanaugcp.tm0gd.mongodb.net/Accounts?retryWrites=true&w=majority",
+    collectionName: 'Sessions'
+  }),
+  cookie: {
+    maxAge: 14000 * 60 * 60 * 24 // Equals 1 day (1 day * 24hr/1 day * 60min/1hr)
+  }
+}));
 
 // make client-side scripts and files accessible
 app.use(express.static('public'));
